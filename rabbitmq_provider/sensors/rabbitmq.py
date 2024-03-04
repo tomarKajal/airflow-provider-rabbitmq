e32ede1b3809795f29191cf15118f6fe67ec3602
tomarKajal <kajal.tomar@philips.com>
2024-03-04 18:05:40 +0530

    added trigger.

--- rabbitmq_provider/sensors/rabbitmq.py
+++ rabbitmq_provider/sensors/rabbitmq.py
@@ -4,6 +4,8 @@ from airflow.utils.decorators import apply_defaults
 from airflow.triggers.temporal import TimeDeltaTrigger
 from datetime import timedelta, datetime
 from rabbitmq_provider.hooks.rabbitmq import RabbitMQHook
+from rabbitmq_provider.triggers import RabbitMQTriggers
+from airflow.exceptions import AirflowException, AirflowProviderDeprecationWarning, AirflowSkipException
 from airflow.configuration import conf
 from typing import Any, Dict, Union
 
@@ -43,21 +45,39 @@ class RabbitMQSensor(BaseSensorOperator):
         else:
             if not self.poke(context=context):
                 self._defer()
+            else:
+                super().execute(context)
+                return self._return_value
+
 
     def _defer(self) -> None:
         self.defer(
-            trigger=TimeDeltaTrigger(delta=timedelta(minutes=5)),
+            #trigger=TimeDeltaTrigger(delta=timedelta(minutes=5)),
+            trigger=RabbitMQTriggers(self.queue_name,self.rabbitmq_conn_id),
             method_name="execute_complete",
         )
 
-    '''def execute_complete(self, context: dict):
-        super().execute(context)
-        return self._return_value'''
-    
-    def execute_complete(self,context: dict,event: Union[Dict[str, Any], None] = None) -> None:
-        logging.info("--- Inside execute complete ----")
-        logging.info(f"printing event: {event}")
-        return
+    def execute_complete(self, context: dict, event: Union[Dict[str, Any], None] = None) -> None:
+        """
+        Execute when a message is received from RabbitMQ.
+
+        Relies on event data to determine success or error.
+        """
+        if event["status"] == "running":
+            # Assuming data is in "data" field of the event
+            data = event.get("data", None)
+            if data:
+                # Process the received data from RabbitMQ (replace with your logic)
+                # ... your processing logic here ...
+                self.log.info("Successfully processed message from RabbitMQ: %s", data)
+            else:
+                self.log.error("Received empty message from RabbitMQ")
+        elif event["status"] == "error":
+            # TODO: remove this if block when min_airflow_version is set to higher than 2.7.1
+            if self.soft_fail:
+                raise AirflowSkipException(event["message"])
+            raise AirflowException(event["message"])
+
 
     def poke(self, context: dict):
         hook = RabbitMQHook(self.rabbitmq_conn_id)

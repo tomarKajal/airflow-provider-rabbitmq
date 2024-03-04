6f7d3c6271d49cba349e4787afd1727797254fa0
tomarKajal <kajal.tomar@philips.com>
2024-03-05 00:04:14 +0530

    correcting identation

--- rabbitmq_provider/triggers/rabbitmq.py
+++ rabbitmq_provider/triggers/rabbitmq.py
@@ -1,6 +1,5 @@
-from typing import Any, AsyncIterator
+from typing import AsyncIterator
 from airflow.triggers.base import BaseTrigger, TriggerEvent
-from airflow.configuration import conf
 from rabbitmq_provider.hooks.rabbitmq import RabbitMQHook
 import asyncio
 
@@ -15,36 +14,35 @@ class RabbitMQTriggers(BaseTrigger):
         self.poke_interval = poke_interval
         self._return_value = None
 
+    def serialize(self):
+        return (
+            "airflow.providers.rabbitmq.triggers.RabbitMQTriggers",
+            {
+                "queue_name": self.queue_name,
+                "rabbitmq_conn_id": self.rabbitmq_conn_id,
+                "poke_interval": self.poke_interval,
+            },
+        )
 
-def serialize(self):
-    return (
-        "airflow.providers.rabbitmq.triggers.RabbitMQTriggers",
-        {
-            "queue_name": self.queue_name,
-            "rabbitmq_conn_id": self.rabbitmq_conn_id,
-            "poke_interval": self.poke_interval,
-        },
-    )
+    async def run(self) -> AsyncIterator[TriggerEvent]:
+        """Asynchronously check for messages in RabbitMQ."""
+        try:
+            while True:
+                if await self.poke(context={}):  # Call the asynchronous poke function
+                    yield TriggerEvent(
+                        {"status": "running", "data": self._return_value}
+                    )
+                else:
+                    self.log.info("No message found in RabbitMQ")
+                await asyncio.sleep(self.poke_interval)
+        except Exception as e:
+            yield TriggerEvent({"status": "error", "message": str(e)})
 
-
-async def run(self) -> AsyncIterator[TriggerEvent]:
-    """Asynchronously check for messages in RabbitMQ."""
-    try:
-        while True:
-            if await self.poke(context={}):  # Call the asynchronous poke function
-                yield TriggerEvent({"status": "running", "data": self._return_value})
+    async def poke(self, context: dict):
+        async with RabbitMQHook(self.rabbitmq_conn_id) as hook:
+            message = await hook.pull(self.queue_name)
+            if message is not None:
+                self._return_value = message
+                return True
             else:
-                self.log.info("No message found in RabbitMQ")
-            await asyncio.sleep(self.poke_interval)
-    except Exception as e:
-        yield TriggerEvent({"status": "error", "message": str(e)})
-
-
-async def poke(self, context: dict):
-    async with RabbitMQHook(self.rabbitmq_conn_id) as hook:
-        message = await hook.pull(self.queue_name)
-        if message is not None:
-            self._return_value = message
-            return True
-        else:
-            return False
+                return False
